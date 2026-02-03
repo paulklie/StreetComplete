cff4a918bd40b56a1f53e7cafba44a62114b2ffb
Helium314 <helium314@mailbox.org>
2026-02-03 17:43:46 +0100

    re-add the level quest extension

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/level/AddLevel.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/level/AddLevel.kt
@@ -1,5 +1,7 @@
 package de.westnordost.streetcomplete.quests.level
 
+import androidx.appcompat.app.AlertDialog
+import android.content.Context
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
 import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
@@ -8,10 +10,12 @@ import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
 import de.westnordost.streetcomplete.data.osm.mapdata.filter
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmElementQuestType
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmQuestController
 import de.westnordost.streetcomplete.data.quest.AndroidQuest
 import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.CITIZEN
 import de.westnordost.streetcomplete.osm.Tags
 import de.westnordost.streetcomplete.osm.isPlace
+import de.westnordost.streetcomplete.quests.questPrefix
 import de.westnordost.streetcomplete.util.math.contains
 import de.westnordost.streetcomplete.util.math.isInMultipolygon
 
@@ -49,6 +53,9 @@ class AddLevel : OsmElementQuestType<String>, AndroidQuest {
     override fun getTitle(tags: Map<String, String>) = R.string.quest_level_title2
 
     override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> {
+        if (prefs.getBoolean(questPrefix(prefs) + PREF_MORE_LEVELS, false))
+            return getApplicableElementsForMoreLevels(mapData)
+
         // get geometry of all malls in the area
         val mallGeometries = mapData
             .filter { mallFilter.matches(it) }
@@ -83,6 +90,8 @@ class AddLevel : OsmElementQuestType<String>, AndroidQuest {
     }
 
     override fun isApplicableTo(element: Element): Boolean? {
+        if (prefs.getBoolean(questPrefix(prefs) + PREF_MORE_LEVELS, false))
+            return isApplicableToForMoreLevels(element)
         if (!filter.matches(element) || !element.isPlace()) return false
         // for shops with no level, we actually need to look at geometry in order to find if it is
         // contained within any multi-level mall
@@ -94,4 +103,91 @@ class AddLevel : OsmElementQuestType<String>, AndroidQuest {
     override fun applyAnswerTo(answer: String, tags: Tags, geometry: ElementGeometry, timestampEdited: Long) {
         tags["level"] = answer
     }
+
+    // allows other buildings as long as they have more than one level
+    private val mallAndMoreFilter by lazy { """
+        ways, relations with
+         shop = mall
+         or aeroway = terminal
+         or railway = station
+         or amenity = bus_station
+         or public_transport = station
+         or (building and building:levels > 1)
+    """.toElementFilterExpression() }
+
+    private val nodesWithoutLevelFilter by lazy { "nodes with !level".toElementFilterExpression() }
+
+    private fun getApplicableElementsForMoreLevels(mapData: MapDataWithGeometry): Iterable<Element> {
+        // get geometry of all malls in the area
+        val geometries = mapData
+            .filter { mallAndMoreFilter.matches(it) }
+            .mapNotNull { mapData.getGeometry(it.type, it.id) as? ElementPolygonsGeometry }
+        if (geometries.isEmpty()) return emptyList()
+
+        val doctors = mapData.filter { it.isDoctorWithoutLevel() }
+        val result = mutableListOf<Element>()
+        doctors.forEach docs@{ doc ->
+            geometries.forEach { geometry ->
+                if (mapData.getGeometry(doc.type, doc.id)?.center?.isInMultipolygon(geometry.polygons) != true)
+                    return@forEach
+                result.add(doc)
+                return@docs
+            }
+        }
+
+        val multiLevelMallGeometries = getMultiLevelMallGeometries(geometries, mapData)
+        if (multiLevelMallGeometries.isEmpty()) return result
+
+        // now, return all shops that have no level tagged and are inside those multi-level malls
+        val shopsWithoutLevel = mapData
+            .filter(nodesWithoutLevelFilter)
+            .filter { it.isPlace() && !it.isDoctorWithoutLevel() }
+            .toMutableList()
+        if (shopsWithoutLevel.isEmpty()) return result
+
+        for (mallGeometry in multiLevelMallGeometries) {
+            val it = shopsWithoutLevel.iterator()
+            while (it.hasNext()) {
+                val shop = it.next()
+                val pos = mapData.getGeometry(shop.type, shop.id)?.center ?: continue
+                if (!mallGeometry.bounds.contains(pos)) continue
+                if (!pos.isInMultipolygon(mallGeometry.polygons)) continue
+
+                result.add(shop)
+                it.remove() // shop can only be in one mall
+            }
+        }
+        return result
+    }
+
+    private fun isApplicableToForMoreLevels(element: Element): Boolean? {
+        if (!nodesWithoutLevelFilter.matches(element) || !element.isPlace()) return false
+        return null
+    }
+
+    override val hasQuestSettings = true
+
+    override fun getQuestSettingsDialog(context: Context): AlertDialog =
+        AlertDialog.Builder(context)
+            .setTitle(R.string.quest_settings_level_title)
+            .setNegativeButton(android.R.string.cancel, null)
+            .setItems(
+                arrayOf(context.getString(R.string.quest_settings_level_default), context.getString(R.string.quest_settings_level_more))
+            ) { _, i ->
+                if (i == 1) prefs.putBoolean(questPrefix(prefs) + PREF_MORE_LEVELS, true)
+                else prefs.remove(questPrefix(prefs) + PREF_MORE_LEVELS)
+                OsmQuestController.reloadQuestTypes()
+            }
+            .create()
+
 }
+
+private fun Element.isDoctorWithoutLevel() = !tags.containsKey("level") && when (tags["amenity"]) {
+    "doctors", "dentist" -> true
+    else -> when (tags["healthcare"]) {
+        "doctor", "dentist", "psychotherapist", "physiotherapist" -> true
+        else -> false
+    }
+}
+
+const val PREF_MORE_LEVELS = "qs_AddLevel_more_levels"
