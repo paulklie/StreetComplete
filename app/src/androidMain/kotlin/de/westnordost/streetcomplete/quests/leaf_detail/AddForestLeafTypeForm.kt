44493bffcaffc797fac54ee19af2fdaad24c070e
Helium314 <helium314@mailbox.org>
2026-02-06 15:07:36 +0100

    fix crash on opening AddForestLeafTypeForm

--- app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/leaf_detail/AddForestLeafTypeForm.kt
+++ app/src/androidMain/kotlin/de/westnordost/streetcomplete/quests/leaf_detail/AddForestLeafTypeForm.kt
@@ -10,7 +10,7 @@ import de.westnordost.streetcomplete.data.osm.mapdata.Node
 
 class AddForestLeafTypeForm : AItemSelectQuestForm<ForestLeafType, ForestLeafType>() {
 
-    override val items = if (element is Node) listOf(ForestLeafType.NEEDLELEAVED, ForestLeafType.BROADLEAVED) else ForestLeafType.entries
+    override val items get() = if (element is Node) listOf(ForestLeafType.NEEDLELEAVED, ForestLeafType.BROADLEAVED) else ForestLeafType.entries
     override val itemsPerRow = 3
     override val serializer = serializer<ForestLeafType>()
 
